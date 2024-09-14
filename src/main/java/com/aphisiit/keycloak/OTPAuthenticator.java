0737321262bc36d18fe77684e3d0a179f69b0eb7
Aphisit Namracha <aphisit.namracha@outlook.com>
2024-09-14 07:59:28 +0700

    [fixed] authentication with OTP

--- src/main/java/com/aphisiit/keycloak/OTPAuthenticator.java
+++ src/main/java/com/aphisiit/keycloak/OTPAuthenticator.java
@@ -1,99 +1,160 @@
 package com.aphisiit.keycloak;
 
-import org.jboss.logging.Logger;
-import org.keycloak.authentication.AuthenticationFlowContext;
-import org.keycloak.authentication.AuthenticationFlowError;
-import org.keycloak.authentication.Authenticator;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserCredentialModel;
-import org.keycloak.models.UserModel;
-import org.keycloak.models.credential.OTPCredentialModel;
-import org.keycloak.services.messages.Messages;
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import jakarta.ws.rs.core.Response;
-
-public class OTPAuthenticator implements Authenticator {
-    
-    private final Logger log = Logger.getLogger(this.getClass());
-
-    @Override
-    public void authenticate(AuthenticationFlowContext context) {
-
-        UserModel user = context.getUser();
-
-        log.info("user.credentialManager().isConfiguredFor(OTPCredentialModel.TYPE): " + user.credentialManager().isConfiguredFor(OTPCredentialModel.TYPE));
-
-        log.info("OTPCredentialModel.TYPE.count(): " + user.credentialManager().getStoredCredentialsByTypeStream(OTPCredentialModel.TYPE).count());
-
-        if (user.credentialManager().isConfiguredFor(OTPCredentialModel.TYPE)) {
-
-            log.info("context.getAuthenticationSession().getAction(): " + context.getAuthenticationSession().getAction());
-            log.info("context.getAuthenticationSession().getParentSession(): " + context.getAuthenticationSession().getParentSession());
-            log.info("context.getAuthenticationSession().getTabId(): " + context.getAuthenticationSession().getTabId());
-
-            Response challenge = Response.status(400) 
-                // .entity("{\"error\": \"otp_required\", \"reference\": \"" + context.getAuthenticationSession().getTabId() + "\"}")
+ import org.keycloak.authentication.AuthenticationFlowContext;
+ import org.keycloak.authentication.AuthenticationFlowError;
+ import org.keycloak.authentication.CredentialValidator;
+import org.keycloak.authentication.authenticators.directgrant.AbstractDirectGrantAuthenticator;
+import org.keycloak.credential.CredentialProvider;
+ import org.keycloak.credential.OTPCredentialProvider;
+ import org.keycloak.events.Errors;
+ import org.keycloak.models.AuthenticationExecutionModel;
+ import org.keycloak.models.KeycloakSession;
+ import org.keycloak.models.RealmModel;
+ import org.keycloak.models.UserCredentialModel;
+ import org.keycloak.models.UserModel;
+ import org.keycloak.models.credential.OTPCredentialModel;
+ import org.keycloak.provider.ProviderConfigProperty;
+ 
+ import jakarta.ws.rs.core.MultivaluedMap;
+ import jakarta.ws.rs.core.Response;
+ import java.util.LinkedList;
+ import java.util.List;
+ 
+ /**
+  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+  * @version $Revision: 1 $
+  */
+ public class OTPAuthenticator extends AbstractDirectGrantAuthenticator implements CredentialValidator<OTPCredentialProvider> {
+ 
+     public static final String PROVIDER_ID = "direct-grant-validate-otp";
+ 
+     @Override
+     public void authenticate(AuthenticationFlowContext context) {
+         if (!configuredFor(context.getSession(), context.getRealm(), context.getUser())) {
+             if (context.getExecution().isConditional()) {
+                 context.attempted();
+             } else if (context.getExecution().isRequired()) {
+                 context.getEvent().error(Errors.INVALID_USER_CREDENTIALS);
+                 Response challengeResponse = errorResponse(Response.Status.UNAUTHORIZED.getStatusCode(), "invalid_grant", "Invalid user credentials");
+                 context.failure(AuthenticationFlowError.INVALID_USER, challengeResponse);
+             }
+             return;
+         }
+         MultivaluedMap<String, String> inputData = context.getHttpRequest().getDecodedFormParameters();
+ 
+         String otp = inputData.getFirst("otp");
+ 
+         // KEYCLOAK-12908 Backwards compatibility. If paramter "otp" is null, then assign "totp".
+         otp = (otp == null) ? inputData.getFirst("totp") : otp;
+ 
+         // Always use default OTP credential in case of direct grant authentication
+         String credentialId = getCredentialProvider(context.getSession())
+                     .getDefaultCredential(context.getSession(), context.getRealm(), context.getUser()).getId();
+ 
+         if (otp == null) {
+             if (context.getUser() != null) {
+                 context.getEvent().user(context.getUser());
+             }
+             context.getEvent().error(Errors.INVALID_USER_CREDENTIALS);
+            //  Response challengeResponse = errorResponse(Response.Status.UNAUTHORIZED.getStatusCode(), "invalid_grant", "Invalid user credentials");
+            Response challengeResponse = Response.status(Response.Status.UNAUTHORIZED.getStatusCode()) 
                 .entity("{\"error\": \"otp_required\", \"reference\": \"" + context.getAuthenticationSession().getTabId() + "\"}")
                 .type("application/json")
-            .build();
-
-            context.challenge(challenge);
-        } else {
-            context.success();
-        }
-    }
-
-    @Override
-    public void action(AuthenticationFlowContext context) {
-        // When the user submits the OTP
-        UserModel user = context.getUser();
-        String otp = context.getHttpRequest().getDecodedFormParameters().getFirst(OTPCredentialModel.TYPE);
-
-        log.info("otp: " + otp);
-
-        if (otp == null || otp.isEmpty()) {
-            context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS,
-                    context.form().setError(Messages.MISSING_TOTP).createErrorPage(Response.Status.BAD_REQUEST));
-            return;
-        }
-
-        // UserCredentialModel.otp(OTPCredentialModel.TYPE, otp);
-
-        // user.credentialManager().isValid(UserCredentialModel.otp(OTPCredentialModel.TYPE, otp));
-        // context.
-        boolean validOtp = user.credentialManager().isValid(UserCredentialModel.otp(OTPCredentialModel.TYPE, otp));
-
-        // boolean isValidOtp = context.getSession().userCredentialManager().isValid(
-        //         context.getRealm(),
-        //         context.getUser(),
-        //         UserCredentialModel.otp(otp)
-        // );
-
-        if (validOtp) {
-            context.success();
-        } else {
-            context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS,
-                    context.form().setError(Messages.INVALID_TOTP).createErrorPage(Response.Status.BAD_REQUEST));
-        }
-    }
-
-    @Override
-    public void close() {
-    }
-
-    @Override
-    public boolean requiresUser() {
-        return true;
-    }
-
-    @Override
-    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
-        // return false;
-        return user.credentialManager().isConfiguredFor(OTPCredentialModel.TYPE);
-    }
-
-    @Override
-    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
-    }
-}
+                .build();
+             context.failure(AuthenticationFlowError.INVALID_USER, challengeResponse);
+             return;
+         }
+         boolean valid = getCredentialProvider(context.getSession()).isValid(context.getRealm(), context.getUser(), new UserCredentialModel(credentialId, OTPCredentialModel.TYPE, otp));
+         if (!valid) {
+             context.getEvent().user(context.getUser());
+             context.getEvent().error(Errors.INVALID_USER_CREDENTIALS);
+            //  Response challengeResponse = errorResponse(Response.Status.UNAUTHORIZED.getStatusCode(), "invalid_grant", "Invalid user credentials");
+            Response challengeResponse = Response.status(Response.Status.UNAUTHORIZED.getStatusCode()) 
+                .entity("{\"error\": \"otp_required\", \"reference\": \"" + context.getAuthenticationSession().getTabId() + "\"}")
+                .type("application/json")
+                .build();
+             context.failure(AuthenticationFlowError.INVALID_USER, challengeResponse);
+             return;
+         }
+ 
+         context.success();
+     }
+ 
+     @Override
+     public boolean requiresUser() {
+         return true;
+     }
+ 
+     @Override
+     public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+         return getCredentialProvider(session).isConfiguredFor(realm, user);
+     }
+ 
+     @Override
+     public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+ 
+     }
+ 
+     @Override
+     public boolean isUserSetupAllowed() {
+         return false;
+     }
+ 
+ 
+     @Override
+     public String getDisplayType() {
+         return "OTP";
+     }
+ 
+     @Override
+     public String getReferenceCategory() {
+         return null;
+     }
+ 
+     @Override
+     public boolean isConfigurable() {
+         return false;
+     }
+ 
+     @Override
+     public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+         return REQUIREMENT_CHOICES;
+     }
+ 
+     @Override
+     public String getHelpText() {
+         return "Validates the one time password supplied as a 'totp' form parameter in direct grant request";
+     }
+ 
+     @Override
+     public List<ProviderConfigProperty> getConfigProperties() {
+         return new LinkedList<>();
+     }
+ 
+     @Override
+     public String getId() {
+         return PROVIDER_ID;
+     }
+ 
+     public OTPCredentialProvider getCredentialProvider(KeycloakSession session) {
+         return (OTPCredentialProvider)session.getProvider(CredentialProvider.class, "keycloak-otp");
+     }
+ 
+ }
